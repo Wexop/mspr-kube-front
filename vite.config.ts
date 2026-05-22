0f878c7d5372fc400a3830e4a31acf22e67a2c03
Wexop <mattheurtin@gmail.com>
2026-05-22 09:17:23 +0200

    feat : add talwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
+import {reactRouter} from "@react-router/dev/vite";
+import {defineConfig} from "vite";
 import devtoolsJson from 'vite-plugin-devtools-json'
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+
 
 export default defineConfig({
-  plugins: [devtoolsJson(), reactRouter(), tsconfigPaths()],
+    plugins: [tailwindcss(),
+        , devtoolsJson(), reactRouter(), tsconfigPaths()],
 });
